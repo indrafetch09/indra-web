867f132c0ac2f96630f171d40f5a98cb43602b6e
Kaiden <harindrawahyu131@gmail.com>
2026-03-03 15:22:15 +0700

    add ascii banner with react component

--- src/components/HeadCmd.jsx
+++ src/components/HeadCmd.jsx
@@ -15,18 +15,18 @@ const HeadCmd = ({ pathname }) => {
         <>
             {/* head */}
             <div className="hidden sm:block">
-                <div class="flex items-center gap-4">
-                    <span class="font-bold text-lg text-accent-tosca">~</span>
+                <div className="flex items-center gap-4">
+                    <span className="font-bold text-lg text-accent-tosca">~</span>
                     {/* horizontal line */}
-                    <div class="grow border-t border-main"></div>
+                    <div className="grow border-t border-main"></div>
                     <div className="flex gap-2 text-sm font-bold text-main">
                         <span className="font-normal">on</span>
-                        <div class="flex gap-1 text-accent-purple">
+                        <div className="flex gap-1 text-accent-purple">
                             <IoMdGitBranch />
                             <span >main</span>
                         </div>
                         <span className="font-normal">via</span>
-                        <div class="flex gap-1 ubderline text-accent-green">
+                        <div className="flex gap-1 ubderline text-accent-green">
                             <FaGithub />
                             <a className="underline" href="https://github.com/indrafetch09">
                                 indrafetch09
@@ -38,9 +38,9 @@ const HeadCmd = ({ pathname }) => {
             </div>
 
             {/* Only shown on mobile */}
-            <div class="flex text-sm gap-2">
-                <span class="font-bold text-accent-green">@</span>
-                <span class="text-(--text-main)">{command}</span>
+            <div className="flex text-sm gap-2">
+                <span className="font-bold text-accent-green">@</span>
+                <span className="text-(--text-main)">{command}</span>
             </div>
         </>
     );
